b522f34b2b4b5517f0450c2a246801be32f837f3
a.elezi68 <a.elezi68@gmail.com>
2016-10-03 04:29:48 -0400

    modification routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :students
 
-  get '/' => "controller#action"
+  root "controller#action"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
