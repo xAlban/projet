c0149ed88e3fea67484fa1db96dc19e5547768b0
a.elezi68 <a.elezi68@gmail.com>
2016-10-15 08:30:06 -0400

    test correction cities controller test

--- test/controllers/cities_controller_test.rb
+++ test/controllers/cities_controller_test.rb
@@ -35,8 +35,10 @@ class CitiesControllerTest < ActionController::TestCase
   end
 
   test "should update city" do
-    patch :update, id: @city, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
-    assert_redirected_to city_path(assigns(:city))
+    VCR.use_cassette("nominatim") do
+      patch :update, id: @city, city: { lat: @city.lat, lon: @city.lon, name: @city.name }
+      assert_redirected_to city_path(assigns(:city))
+    end
   end
 
   test "should destroy city" do
