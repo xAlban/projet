fddcf611516199afa4619424a511f04fba68243d
a.elezi68 <a.elezi68@gmail.com>
2016-10-26 06:23:52 -0400

    modification view1 ajout /app

--- public/angular-seed/app/app.js
+++ public/angular-seed/app/app.js
@@ -6,9 +6,9 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: 'view1'});
 }]);
--- public/angular-seed/app/view1/view1.js
+++ public/angular-seed/app/view1/view1.js
@@ -4,14 +4,14 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
+    templateUrl: 'app/view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
 
 .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
   $scope.students = [];
-  $http.get('/students/:id.json').success(function(data){
+  $http.get('/students.json').success(function(data){
     $scope.students = data;
   });
   
