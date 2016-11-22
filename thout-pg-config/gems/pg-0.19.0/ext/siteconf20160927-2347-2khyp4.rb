b74231a7c0490b99850431178476d36e8c78beaf
a.elezi68 <a.elezi68@gmail.com>
2016-11-22 08:54:00 -0500

    delete dossier inutile

--- thout-pg-config/gems/pg-0.19.0/ext/siteconf20160927-2347-2khyp4.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20160927-2347-g5mls4"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
